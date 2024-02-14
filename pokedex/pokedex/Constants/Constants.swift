4665444f115a31e16c265a0e34118c0a540fcb0e
nahuel-hello <n.bula@hellohello.is>
2024-02-14 11:52:29 -0300

    feature/search_pokemons_service

---
+++ pokedex/pokedex/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Nahuel Bula on 13/2/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let host: String = "https://pokeapi.co/api/v2"
+    static let pageSize: Int = 20
+}
